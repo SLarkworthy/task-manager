cb632b117899535abed5d0f65df7e19f5d1b01dc
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-09-02 10:55:47 -0400

    file restructuring

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(password){
+            if(password.includes("password")){
+                throw new Error("Passoword cannot contain the word passoword")
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0){
+                throw new Error("age must be a positive number");
+            }
+        }
+    }
+})
+
+module.exports = User;
