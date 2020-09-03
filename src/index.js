5c43092348cd821d1fae89e49711eef79732c9c4
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-09-03 14:40:39 -0400

    task get routes

--- src/index.js
+++ src/index.js
@@ -14,9 +14,33 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((e) => {
         res.status(400).send(e);
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}) //fetches all users stored in the db and returns a promise
+    .then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
+// req.params has the url params
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user); //status will be 200 by default
+    }).catch((e) => {
+        res.status(500).send();
     })
 })
 
@@ -24,9 +48,33 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
     task.save().then(() => {
-        res.send(task);
+        res.status(201).send(task);
     }).catch((e) => {
         res.status(400).send(e);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}) //fetches all users stored in the db and returns a promise
+    .then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task); //status will be 200 by default
+    }).catch((e) => {
+        res.status(500).send();
     })
 })
 
