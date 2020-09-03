1d48c37963b77b796d1b618122044477efd97164
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-09-03 10:14:52 -0400

    moved task to its own file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,14 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-const Task = mongoose.model('Tasks', {
-    description: { 
-        type: String,
-        required: true,
-        trim: true
-    },
-    status: {
-        type: Boolean,
-        default: false
-    }
-})
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
+
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,8 +16,17 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         res.send(user);
     }).catch((e) => {
-        res.status(400);
-        res.send(e);
+        res.status(400).send(e);
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
     })
 })
 
