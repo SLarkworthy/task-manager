a51ce6f521507c7e3c11bd501f4836853c0bdc30
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-09-19 16:05:59 -0400

    check for logged in user on get route

--- src/routers/task.js
+++ src/routers/task.js
@@ -26,11 +26,12 @@ router.get('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     
     try {
-        const task = await Task.findById(_id)
+        // find one will search by any params you add, so it will make sure the task matches with both.
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if (!task) {
             return res.status(404).send();
         }
