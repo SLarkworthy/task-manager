59cc3a2d10d81f3a1d06b96fddcdd66d734d9df7
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-09-21 12:24:53 -0400

    added multer

--- src/index.js
+++ src/index.js
@@ -8,18 +8,7 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     } else {
-//         next() //must call this when the middleware function is done
-//     }
-// })
 
-//maintenance mode
-// app.use((req, res, next) => {
-//     res.status(503).send('Site in maintenance, try again later');
-// })
 
 app.use(express.json()) //parses incoming json to an object
 app.use(userRouter)
@@ -29,23 +18,3 @@ app.listen(port, () => {
     console.log("Server is up on port " + port);
 })
 
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'dummy123' }, 'thisismynewcourse', { expiresIn: '7 days' });
-    console.log(token);
-
-    const data = jwt.verify(token, 'thisismynewcourse');
-    console.log(data);
-}
-
-myFunction();
-
-const main = async () => {
-    // const task = await Task.findById('5f66609ef2655b2ae4921ecf');
-    // await task.populate('owner').execPopulate();//finds the user and makes task.owner the entire owner
-    // console.log(task);
-
-
-}
-main();
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+const multer = require('multer')
 
 //add middleware method as the second argument
 router.post('/users', async (req, res) => {
@@ -81,6 +82,14 @@ router.patch('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(400).send(e);
     }
+})
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
 })
 
 
